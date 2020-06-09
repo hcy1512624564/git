e670c300eafafa15f20202074063d6a19012b1ce
huang <huang@qq.com>
2020-06-09 12:19:47 +0800

    commit3

--- 0608/11.php
+++ 0608/11.php
@@ -9,3 +9,19 @@ while(xxxx){
 	xxxx
 }*/
 
+
+$i = 1;
+for(;;){
+    if($i>10){
+    break;
+    }
+    echo $i."<br/>";
+    $i++;
+}
+echo "<hr/>";
+
+$j = 1;
+while($j<=10){
+    echo $j."<br/>";
+    $j++;
+}
